b54c896e720d288da460f70ea5af1679b188b801
HLche <e4e511@gmail.com>
2020-04-08 02:55:20 +0800

    CHL-portfolio-v0.0.1

---
+++ script/general.js
@@ -0,0 +1,25 @@
+export const scrollAnimation = (animationList)=>{
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0) {
+                console.log(entry);
+                const anim = animationList[entry.target.id].anim;
+                if (anim !== undefined) {
+                    entry.target.style.animation = anim;
+                    observer.unobserve(entry.target);
+                } else {
+                    console.log(
+                        "Error - id not exist in animationList"
+                    );
+                }
+            }
+        });
+    });
+    Object.keys(animationList).map((animObject, index) => {
+        observer.observe(animationList[animObject].targetElement);
+    });
+}
+
+export const tester = ()=>{
+    console.log("tester");
+}
